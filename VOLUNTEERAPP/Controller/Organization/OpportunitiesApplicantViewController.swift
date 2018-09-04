c30d89f1672ba589daad7ac8e815e166134222fc
Klaudius Ivan <klaudsvanz@gmail.com>
2018-09-04 10:35:31 +0700

    no message

---
+++ VOLUNTEERAPP/Controller/Organization/OpportunitiesApplicantViewController.swift
@@ -0,0 +1,50 @@
+//
+//  OpportunitiesApplicantViewController.swift
+//  VOLUNTEERAPP
+//
+//  Created by Klaudius Ivan on 03/09/18.
+//  Copyright © 2018 Klaudius. All rights reserved.
+//
+
+import UIKit
+
+class OpportunitiesApplicantViewController: UIViewController {
+    //MARK - Outlets
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension OpportunitiesApplicantViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
+
+
