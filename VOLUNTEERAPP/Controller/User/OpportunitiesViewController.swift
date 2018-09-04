8a6bd82af4c4d00f518ba73a2b78d0f153f5c483
Cason Kang <cason_san@hotmail.com>
2018-09-04 16:22:55 +0700

    added load data from firebase for opportunities

--- VOLUNTEERAPP/Controller/User/OpportunitiesViewController.swift
+++ VOLUNTEERAPP/Controller/User/OpportunitiesViewController.swift
@@ -7,31 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class OpportunitiesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    var ref:DatabaseReference!
+    var opportunities:[Opportunity]=[]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return namaEventOpportunities.count
+        return opportunities.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = opportunitiesTable.dequeueReusableCell(withIdentifier: "OpportunitiesCell", for: indexPath) as? OpportunitiesTableViewCell
-        
-        cell?.gambarOpportunitiesCell.image = gambarEventOpportunities[indexPath.row]
-        cell?.namaEventOpportunitiesCell.text = namaEventOpportunities[indexPath.row]
-        cell?.tanggalEventOpportunitiesCell.text = tanggalEventOpportunities[indexPath.row]
-        cell?.neededEventOpportunitiesCell.text = neededEventOpportunities[indexPath.row]
-        
+        let totalVol = opportunities[indexPath.row].dapur + opportunities[indexPath.row].medis + opportunities[indexPath.row].sar
+        cell!.gambarOpportunitiesCell.image = gambarEventOpportunities[1]
+        cell!.namaEventOpportunitiesCell.text = opportunities[indexPath.row].namaOpportunity
+        cell!.tanggalEventOpportunitiesCell.text = "\(opportunities[indexPath.row].tanggalMulai) - \(opportunities[indexPath.row].tanggalSelesai)"
+        cell!.neededEventOpportunitiesCell.text = "\(totalVol) Volunteer/s needed"
         return cell!
     }
     
     var clickedIndex : Int = 0
     
-    var namaEventOpportunities : [String] = ["Gempa Lombok","Gempa Jogja"]
-    
+//    var namaEventOpportunities : [String] = ["Gempa Lombok","Gempa Jogja"]
+//
     var tanggalEventOpportunities : [String] = ["16 Agustus - 16 September","1 Agustus - 20 Agustus"]
-    
-    var neededEventOpportunities : [String] = ["5 Volunteer Needed","8 Volunter Needed"]
-    
+//
+//    var neededEventOpportunities : [String] = ["5 Volunteer Needed","8 Volunter Needed"]
+//
     var gambarEventOpportunities : [UIImage] = [
         UIImage(named: "GempaLombok")!,
         UIImage(named: "GempaJogja")!
@@ -59,8 +62,28 @@ class OpportunitiesViewController: UIViewController,UITableViewDelegate,UITableV
     @IBOutlet weak var opportunitiesTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+            self.loadDataFromFirebase()
         
         // Do any additional setup after loading the view.
+    }
+    
+    func loadDataFromFirebase() {
+        self.ref.child("opportunities").observe(.childAdded) { (snapshot) in
+            let x = snapshot.value as! [String: Any]
+            
+            let a = Opportunity(
+                    namaOpportunity: x["namaOpportunity"] as! String,
+                    tanggalMulai: x["tanggalMulai"] as! String,
+                    tanggalSelesai: x["tanggalSelesai"] as! String,
+                    tempat: x["tempat"] as! String,
+                    sar: x["sar"] as! Int,
+                    medis: x["medis"] as! Int,
+                    dapur: x["dapur"] as! Int
+            )
+            self.opportunities.append(a)
+            self.opportunitiesTable.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
