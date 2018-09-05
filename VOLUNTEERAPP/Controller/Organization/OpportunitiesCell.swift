fda22ab3c08482041e5fde267986809f13e9cdd7
Klaudius Ivan <klaudsvanz@gmail.com>
2018-09-05 10:32:20 +0700

    no message

--- VOLUNTEERAPP/Controller/Organization/OpportunitiesCell.swift
+++ VOLUNTEERAPP/Controller/Organization/OpportunitiesCell.swift
@@ -20,7 +20,18 @@ class OpportunitiesCell: UITableViewCell{
     
     @IBOutlet weak var statusOpportunitiesLabel: UILabel!
     
-    
+    @IBOutlet weak var backView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backView.layer.cornerRadius = 3
+        
+        self.backView.layer.shadowOpacity = 0.5
+        self.backView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        self.backView.layer.shadowRadius = 2
+        self.backView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.backView.layer.masksToBounds = false
+    }
     func setOpportunitiesCell() {
         
     }
