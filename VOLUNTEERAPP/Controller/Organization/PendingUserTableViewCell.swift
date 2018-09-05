4f7e073053fe0df0d30e9966ea67dc4b90f3250f
Klaudius Ivan <klaudsvanz@gmail.com>
2018-09-05 11:22:41 +0700

    no message

--- VOLUNTEERAPP/Controller/Organization/PendingUserTableViewCell.swift
+++ VOLUNTEERAPP/Controller/Organization/PendingUserTableViewCell.swift
@@ -17,9 +17,17 @@ class PendingUserTableViewCell: UITableViewCell {
     
     @IBOutlet weak var skillPendingUserCell: UILabel!
     
+    @IBOutlet weak var backView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.backView.layer.cornerRadius = 3
+        
+        self.backView.layer.shadowOpacity = 0.5
+        self.backView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        self.backView.layer.shadowRadius = 2
+        self.backView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.backView.layer.masksToBounds = false
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
